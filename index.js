0f8c4906b2329259354868ef515d20c269d1d293
Makafui Naaman <makafuinaaman@Makafuis-MacBook-Pro.local>
2019-09-04 17:10:14 -0400

    update

--- index.js
+++ index.js
@@ -6,4 +6,10 @@ app.get('/', (req, res) => res.send('Hello Makafui'))
 
 app.get('/add-todo', (req, res) => res.send('Todo Added'))
 
+app.get('/delete-todo', (req, res) => res.send('Todo Deleted'))
+
+app.get('/mark-todo', (req, res) => res.send('Todo Marked'))
+
+app.get('/display-todo', (req, res) => res.send('Todo Displayed'))
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
